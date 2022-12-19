399f25a0eebe7a3f37292d3f44b892ee6a478742
Cuong Nguyen <75366526+ncuong0704@users.noreply.github.com>
2022-12-19 16:37:27 +0700

    Update form.js

--- js/form.js
+++ js/form.js
@@ -22,7 +22,7 @@ var countLife = 1;
 // biến win để bk người chơi đã thắng r
 var win = false;
 // scrowwin là số điểm để chiến thắng
-var scoreWin = 3000;
+var scoreWin = 100;
 // là mạng hiện tại của người chơi khi chiến thắng
 var currentLife;
 // số mạng dc hồi sinh khi điền form
